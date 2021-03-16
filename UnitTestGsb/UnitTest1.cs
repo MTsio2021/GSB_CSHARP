35c61389af88687d5456c71000b50cb66bdc4fab
MTsio2021 <70960682+MTsio2021@users.noreply.github.com>
2021-03-16 16:38:14 +0100

    Update UnitTest1.cs

--- UnitTestGsb/UnitTest1.cs
+++ UnitTestGsb/UnitTest1.cs
@@ -7,9 +7,13 @@ namespace UnitTestGsb
     [TestClass]
     public class UnitTest1
     {
+        GestionDate date = new GestionDate();
         [TestMethod]
         public void TestMethod1()
         {
+
+            Assert.AreEqual("202102", date.moisPrecedent(), "Erreur");
+            Assert.AreEqual("202104", date.moisSuivant(), "Erreur");
         }
     }
 }
