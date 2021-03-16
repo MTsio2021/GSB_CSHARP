b02056d7e72abce2a20c19ef58457c13613694be
MTsio2021 <70960682+MTsio2021@users.noreply.github.com>
2021-03-16 16:25:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GSB_CSHARP
+ 
---
+++ UnitTestGsb/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGsb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestGsb")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc2ce5cb-d98b-42aa-b41d-d784e6115d41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
